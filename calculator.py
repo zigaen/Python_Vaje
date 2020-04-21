7f325be3c484d72aaf8042d81f4a8f46433eadbd
ZigaE <zigaen@gmail.com>
2020-04-21 22:16:21 +0200

    Kalkulator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+cifra1 = int(input("Vnesi prvo številko!:"))
+cifra2 = int(input("Vnesi drugo številko!:"))
+racun = (input("Vnesi željeno računsko operacijo, izbiraš lahko med seštevanjem +, odštevanjem - , množenjem * in deljenjem / ! "))
+rezultat = float(cifra1 / cifra2)
+rezultat = round(rezultat,3)
+
+if racun == "+":
+    print("Rezultat je:", cifra1 + cifra2)
+elif racun == "-":
+    print("Rezultat je:", cifra1 - cifra2)
+elif racun == "*":
+    print("Rezultat je:", cifra1 * cifra2)
+elif racun == "/":
+    print("Rezultat je:", rezultat)
+else:
+    print("Vnesel nisi pravilne operacije, poizkusi znova!")
