40dc7d7af280912aee28bc799ff5f0b77bbe5c54
ZigaE <zigaen@gmail.com>
2020-04-29 06:08:23 +0200

    Calculation is moved in to elif racun == "/"

--- calculator.py
+++ calculator.py
@@ -1,8 +1,7 @@
 cifra1 = int(input("Vnesi prvo številko!:"))
 cifra2 = int(input("Vnesi drugo številko!:"))
 racun = (input("Vnesi željeno računsko operacijo, izbiraš lahko med seštevanjem +, odštevanjem - , množenjem * in deljenjem / ! "))
-rezultat = float(cifra1 / cifra2)
-rezultat = round(rezultat,3)
+
 
 if racun == "+":
     print("Rezultat je:", cifra1 + cifra2)
@@ -11,6 +10,8 @@ elif racun == "-":
 elif racun == "*":
     print("Rezultat je:", cifra1 * cifra2)
 elif racun == "/":
+    rezultat = float(cifra1 / cifra2)
+    rezultat = round(rezultat, 3)
     print("Rezultat je:", rezultat)
 else:
     print("Vnesel nisi pravilne operacije, poizkusi znova!")
