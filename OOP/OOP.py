89e600af137cb05d07fd4e82cc14e260f654c082
ZigaE <zigaen@gmail.com>
2020-05-14 16:55:53 +0200

    Upgrade to option between two different games.

--- OOP/OOP.py
+++ OOP/OOP.py
@@ -25,40 +25,69 @@ class FootballPlayer(Player):
         self.yellow_cards = yellow_cards
         self.red_cards = red_cards
 
-lebron = BasketballPlayer(first_name="Lebron", last_name="James", height_cm=203, weight_kg=113, points=27.2, rebounds=7.4, assists=7.2)
+answer_game = input("Add player forr football(f) or. basketball (b)? : ")
+if answer_game == "b":
+    while True:
+        answer = input("Let's add a player to the list. Would you like to do that? (y/n): ")
+        answer = answer.lower()
+        if answer == "n":
+            print("Ok, bye!")
+        elif answer == "y":
+            myPlayer = BasketballPlayer(
+                first_name= input("Type first name of the player: "),
+                last_name= input("Type last name of the player: "),
+                height_cm= input("Player height: "),
+                weight_kg= input("Player weight: "),
+                points= input("The number of points: "),
+                rebounds= input("The number of rebounds: "),
+                assists= input("The number of assists: ")
+            )
+            print(f"{myPlayer.first_name} {myPlayer.last_name}   Will be stored in to database!")
+            myPlayer = myPlayer.__dict__
 
-kev_dur = BasketballPlayer(first_name="Kevin", last_name="Durant", height_cm=210, weight_kg=108, points=27.2, rebounds=7.1, assists=4)
+            with open("player_list_basketball.txt", "r") as player_file:
+                player_list = json.loads(player_file.read())
+                print("Added player is: " + str(myPlayer))
+                player_list.append(myPlayer)
+            with open("player_list_basketball.txt", "w") as player_file:
+                    player_file.write(json.dumps(player_list))
+            continue
 
-ronaldo = FootballPlayer(first_name="Cristiano", last_name="Ronaldo", height_cm=184, weight_kg=79, goals=586, yellow_cards=95, red_cards=11)
+        else:
+            print("enter the rigt letter (y/n)")
+        break
+elif answer_game == "f":
+    while True:
+        answer = input("Let's add a player to the list. Would you like to do that? (y/n): ")
+        answer = answer.lower()
+        if answer == "n":
+            print("Ok, bye!")
+        elif answer == "y":
+            myPlayer = FootballPlayer(
+                first_name=input("Type first name of the player: "),
+                last_name=input("Type last name of the player: "),
+                height_cm=input("Player height: "),
+                weight_kg=input("Player weight: "),
+                goals=input("The number of goals: "),
+                yellow_cards=input("The number of yellow cards: "),
+                red_cards=input("The number of red cards: ")
+            )
+            print(f"{myPlayer.first_name} {myPlayer.last_name}   Will be stored in to database!")
+            myPlayer = myPlayer.__dict__
 
-messi = FootballPlayer(first_name="Lionel", last_name="Messi", height_cm=170, weight_kg=67, goals=575, yellow_cards=67, red_cards=0)
+            with open("player_list_football.txt", "r") as player_file:
+                player_list = json.loads(player_file.read())
+                print("Added player is: " + str(myPlayer))
+                player_list.append(myPlayer)
+            with open("player_list_football.txt", "w") as player_file:
+                player_file.write(json.dumps(player_list))
+            continue
 
-answer = input("Let's add a player to the list. Would you like to do that? (y/n): ")
-answer = answer.lower()
-if answer == "n":
-    print("Ok, bye!")
-elif answer == "y":
-    myPlayer = BasketballPlayer(
-        first_name= input("Type first name of the player: "),
-        last_name= input("Type last name of the player: "),
-        height_cm= input("Player height: "),
-        weight_kg= input("Player weight: "),
-        points= input("The number of points: "),
-        rebounds= input("The number of rebounds: "),
-        assists= input("The number of assists: ")
-    )
-    print(f"{myPlayer.first_name} {myPlayer.last_name}   Will be stored in to database!")
-    myPlayer = myPlayer.__dict__
-
-    with open("player_list.txt", "r") as player_file:
-        player_list = json.loads(player_file.read())
-        print("Added player is: " + str(myPlayer))
-        player_list.append(myPlayer)
-    with open("player_list.txt", "w") as player_file:
-            player_file.write(json.dumps(player_list))
-
+        else:
+            print("enter the rigt letter (y/n)")
+        break
 else:
-    print("enter the rigt letter (y/n)")
+    print("You must choose between b/f! ")
 
 
 
